a004176c9c5ce0b3b50dbebf64a4c9a49e4e364d
james <hexaplay2@gmail.com>
2025-03-09 13:09:33 +0100

    init

---
+++ DtoGenerator/ObjectSpecification.cs
@@ -0,0 +1,29 @@
+﻿#nullable enable
+using System.Collections.Generic;
+
+namespace SourceGenerator;
+
+internal struct ObjectSpecification
+{
+    internal DtoType DtoType { get; set; }
+    internal string Name { get; set; }
+    internal List<PropertySpecification> Properties { get; set; }
+}
+
+internal struct PropertySpecification
+{
+    internal string Name { get; set; }
+    internal string Type { get; set; }
+    internal string Summary { get; set; }
+    internal bool IsRequired { get; set; }
+    internal bool IsNullable { get; set; }
+    internal MappingSpecification? MapTo { get; set; }
+    internal MappingSpecification? MapFrom { get; set; }
+    internal DtoType Visibility { get; set; }
+}
+
+internal class MappingSpecification
+{
+    internal string ToType { get; set; }
+    internal string Function { get; set; }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+﻿# Dto Generator
+
+Stop writing boilerplate code for your DTOs. This package will generate them for you.
+
